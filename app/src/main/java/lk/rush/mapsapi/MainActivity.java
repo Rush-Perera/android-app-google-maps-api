44b92b17cdbad7c12c8b11926d5e081679b21adf
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-11-17 00:10:18 +0530

    initial

---
+++ app/src/main/java/lk/rush/mapsapi/MainActivity.java
@@ -0,0 +1,40 @@
+package lk.rush.mapsapi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng latLng = new LatLng(6.9076795, 79.437533);
+        map.addMarker(new MarkerOptions().position(latLng).title("Location"));
+
+//        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,100));
+    }
+}
